0283785de350465af991e004660bdffd456fcf8d
akashsinha501 <akashkr501@gmail.com>
2025-09-26 05:29:11 +0530

    microservice code changes

---
+++ section6/v1-springboot/cards/src/main/java/com/capgemini/cards/repository/CardsRepository.java
@@ -0,0 +1,17 @@
+package com.capgemini.cards.repository;
+
+import com.capgemini.cards.entity.Cards;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CardsRepository extends JpaRepository<Cards, Long> {
+
+    Optional<Cards> findByMobileNumber(String mobileNumber);
+
+    Optional<Cards> findByCardNumber(String cardNumber);
+
+}
+
---
+++ section6/v2-spring-cloud-config/accounts/src/main/java/com/capgemini/accounts/dto/AccountContactInfoDto.java
@@ -0,0 +1,9 @@
+package com.capgemini.accounts.dto;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+import java.util.Map;
+@ConfigurationProperties(prefix = "accounts")
+public record AccountContactInfoDto(String message, Map<String,String>contactDetails, List<String> onCallSupport) {
+}
